3957c516cb59cb60dd0cc80ffaa2102bf5a04f2b
Tuomo Hiippala <tuomo.hiippala@iki.fi>
2019-03-11 09:15:55 +0200

    update init_env.sh

--- init_env.sh
+++ init_env.sh
@@ -12,4 +12,7 @@ export GIT_COMMITTER_EMAIL=anon@localhost
 git clone https://github.com/thiippal/ENG-3072/
 
 # Go to notebooks folder
-cd /home/jovyan/work/ENG-3072/
+cd /home/jovyan/work
+
+# Get course materials.
+./get_materials.sh
